6e792ef76204c60e872e2ffedc405ec7976b5c6a
sunday <2621949203@qq.com>
2022-03-11 16:40:26 +0800

    feat(priority-worker-runner): add priority-worker-runner

---
+++ src/pages/priority-worker-runner/TaskRunner.ts
@@ -0,0 +1,111 @@
+import PriorityQueue from './PriorityQueue';
+
+interface Worker<T> {
+  task: T;
+  priority: number;
+  id: number;
+  timeoutId?: number;
+  onResolve: (res: any) => void;
+  onReject: (errorReason: any) => void;
+}
+
+interface TaskRunnerProps {
+  concurrentExecuteTaskMax: number;
+  timeout?: number;
+}
+
+type PromiseHandleType<T> = (worker: Worker<T>) => Promise<any>;
+
+class TaskRunner<T> {
+  queue: any;
+  promiseHandle: PromiseHandleType<T>;
+  MAX_TASK_NUM: any;
+  processingTask: Worker<T>[];
+  timeout: number | undefined;
+  isProcessing: number;
+  historyWorkerCount: number;
+
+  constructor(promiseHandle: PromiseHandleType<T>, props: TaskRunnerProps) {
+    const { concurrentExecuteTaskMax, timeout } = props;
+    this.queue = new PriorityQueue<Worker<T>>((a, b) => {
+      return a.priority - b.priority;
+    });
+
+    this.promiseHandle = promiseHandle;
+    this.MAX_TASK_NUM = concurrentExecuteTaskMax;
+    this.timeout = timeout;
+
+    this.processingTask = [];
+    this.isProcessing = 0;
+    this.historyWorkerCount = 0;
+  }
+
+  waitForTimeout(worker: Worker<T>) {
+    return new Promise((resolve, reject) => {
+      worker.timeoutId = window.setTimeout(() => {
+        reject('task execute timeout');
+      }, this.timeout);
+    });
+  }
+
+  check() {
+    while (this.isProcessing < this.MAX_TASK_NUM && this.queue.size()) {
+      const worker = this.queue.deq();
+      const { id: runningId, onResolve, onReject } = worker;
+
+      this.isProcessing++;
+      this.processingTask.push(worker);
+
+      const taskPromise = this.promiseHandle(worker);
+
+      const onTaskEnd = () => {
+        clearTimeout(worker.timeoutId);
+        this.isProcessing--;
+        this.processingTask = this.processingTask.filter(({ id }) => id !== runningId);
+        this.check();
+      };
+
+      const race = [taskPromise];
+
+      if (this.timeout) {
+        race.push(this.waitForTimeout(worker));
+      }
+
+      Promise.race(race)
+        .then((res) => {
+          onResolve && onResolve(res);
+        })
+        .catch((res) => {
+          onReject && onReject(res);
+        })
+        .finally(onTaskEnd);
+    }
+  }
+
+  postTask(
+    task: T,
+    {
+      onResolve,
+      onReject,
+      priority,
+    }: {
+      onResolve?: Worker<T>['onResolve'];
+      onReject?: Worker<T>['onReject'];
+      priority: Worker<T>['priority'];
+    },
+  ) {
+    if (!priority) {
+      throw new Error('priority not set');
+    }
+    this.queue.enq({
+      task,
+      priority,
+      id: this.historyWorkerCount++,
+      onResolve,
+      onReject,
+    });
+    this.check();
+  }
+}
+
+export default TaskRunner;
---
+++ src/pages/priority-worker-runner/index.tsx
@@ -0,0 +1,63 @@
+import TaskRunner from './TaskRunner';
+
+interface Task {
+  event: 'timeout' | 'fetch';
+  payload?: any;
+}
+
+const timeoutTask = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('hello world');
+    }, 10);
+  });
+};
+
+const runner = new TaskRunner<Task>(
+  async ({ task }) => {
+    const event = task.event;
+    if (event === 'timeout') {
+      return await timeoutTask();
+    }
+  },
+  {
+    concurrentExecuteTaskMax: 5,
+    timeout: 5,
+  },
+);
+
+runner.postTask(
+  {
+    event: 'timeout',
+  },
+  {
+    priority: 100,
+    onResolve: (result) => {
+      console.log(result);
+    },
+    onReject: (errorReason) => {
+      console.error(errorReason);
+    },
+  },
+);
+
+runner.postTask(
+  {
+    event: 'timeout',
+  },
+  {
+    priority: 100,
+    onResolve: (result) => {
+      console.log(result);
+    },
+    onReject: (errorReason) => {
+      console.error(errorReason);
+    },
+  },
+);
+
+const PriorityWorkerRunner = () => {
+  return <div>priority-worker-runner</div>;
+};
+
+export default PriorityWorkerRunner;
